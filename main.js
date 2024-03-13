573ffe749a06df885f8a06de1990d7b1a1d9b0e3
Ilsa. <ilsasyeda@gmail.com>
2024-03-13 13:05:51 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var guest = [
+    "miral",
+    "sundas",
+    "urooj",
+    "alla dawas",
+    "umair",
+    "ali",
+    "amal",
+    "bilal",
+];
+console.log(guest);
+//after removing guest from the list
+guest.splice(0, 3); //splice method
+guest.splice(2, 3);
+console.log(guest);
